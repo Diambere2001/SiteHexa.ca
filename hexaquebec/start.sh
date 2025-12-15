1973da4535d26e0966aeae81d4923339e10700ca
HexaQuebec <hexaquebec80@gmail.com>
2025-12-15 06:35:32 +0100

    Make start.sh executable

--- hexaquebec/start.sh
+++ hexaquebec/start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-export DJANGO_SETTINGS_MODULE=hexaquebec.settings
-python manage.py collectstatic --noinput
-python manage.py migrate
-gunicorn hexaquebec.wsgi:application --bind 0.0.0.0:$PORT
+set -e
 
+export DJANGO_SETTINGS_MODULE=hexaquebec.settings
+
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+
+gunicorn hexaquebec.wsgi:application --bind 0.0.0.0:$PORT
